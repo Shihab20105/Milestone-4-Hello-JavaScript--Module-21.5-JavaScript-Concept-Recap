1bfb15fb78cad6e7dc60277eea3a98dad2ece967
shihab20105 <shihabur10@gmail.com>
2024-11-07 22:53:25 +0000

    Explored JavaScript concepts and apply revision with four challenges

---
+++ 21_5-1 JavaScript concepts and apply revis/crackerz.js
@@ -0,0 +1,5 @@
+/*                          21_5-1 JavaScript concepts and apply revision with four challenges */
+const reverseMaker = (word)=> {
+    //Write Your solution Here
+    return word.split('').reverse().join('')
+  };
